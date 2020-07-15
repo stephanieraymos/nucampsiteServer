4bce0b9f9adaaef081ec25abac22be4c76347e23
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-15 16:52:18 -0700

    Added if statement to check if client is authenticated + added cookie

--- app.js
+++ app.js
@@ -40,58 +40,59 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    const err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
+  if (!req.signedCookies.user) { //signedCookies automatically parses a signed cookie from the request. Returns false if cookie is not properly signed. If incoming req doesn't include the signedCookies.user property or if signedCookie value is false --> client hasn't been authenticated.
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
+
+    const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    //Grabbing the username and password out from the auth array:
+    const user = auth[0];
+    const pass = auth[1];
+    //Basic validation:
+    if (user === 'admin' && pass === 'password') {
+      res.cookie('user', 'admin', {signed: true});
+      return next(); // authorized
+    } else {
+      const err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
+    }
   }
 
-  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  //Grabbing the username and password out from the auth array:
-  const user = auth[0];
-  const pass = auth[1];
-  //Basic validation:
-  if (user === 'admin' && pass === 'password') {
-    return next(); // authorized
-  } else {
-    const err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    return next(err);
-  }
-}
+  app.use(auth);
 
-app.use(auth);
+  app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')));
+  //ROUTERS
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
+  app.use('/campsites', campsiteRouter);
+  app.use('/promotions', promotionRouter);
+  app.use('/partners', partnerRouter);
 
-//ROUTERS
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+  module.exports = app;
