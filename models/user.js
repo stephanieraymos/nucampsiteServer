635c8aeff19ad1e48fc11b9e6d18277fe2b604ca
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-15 17:38:36 -0700

    Updated user to support user registration, login, and logout in the usersRouter

--- models/user.js
+++ models/user.js
@@ -2,19 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    admin: {
-        type: Boolean,
-        default: false
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  admin: {
+    type: Boolean,
+    default: false
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
