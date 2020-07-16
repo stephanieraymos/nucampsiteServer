1bdbda883bb7651536721f071e2bda51d2f0278a
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-15 18:27:07 -0700

    Comment for users router

--- routes/users.js
+++ routes/users.js
@@ -8,31 +8,34 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup', (req, res, next) => {
-  User.findOne({ username: req.body.username })
+//USER SIGNUP
+router.post('/signup', (req, res, next) => { //Allows new user to register on this website
+  User.findOne({ username: req.body.username }) //Checking if username is already taken
     .then(user => {
-      if (user) {
-        const err = new Error(`User ${req.body.username} already exists!`);
-        err.status = 403;
-        return next(err);
+      if (user) { //A user doc was found with a matching name
+        const err = new Error(`Username ${req.body.username} already exists!`);
+        err.status = 403; //Forbidden
+        return next(err); //Passing to express to handle the error with the next function
       } else {
         User.create({
           username: req.body.username,
           password: req.body.password
+          //Leaving admin out so clients can't make themselves admins (default value is false and it is not required)
         })
           .then(user => {
             res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
-            res.json({ status: 'Registration Successful!', user: user });
+            res.json({ status: 'Woo-hoo! Registration Successful!', user: user });
           })
           .catch(err => next(err));
       }
     })
-    .catch(err => next(err));
+    .catch(err => next(err)); //if findOne returns a rejected promise
 });
 
+//USER LOGIN
 router.post('/login', (req, res, next) => {
-  if (!req.session.user) {
+  if (!req.session.user) { //If user is not already logged in (If there's not a current session for this user)
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
@@ -42,21 +45,22 @@ router.post('/login', (req, res, next) => {
       return next(err);
     }
 
+    //HANDLE USER LOGIN
     const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     const username = auth[0];
     const password = auth[1];
 
-    User.findOne({ username: username })
+    User.findOne({ username: username }) //Checking inputted username against the user docs that we have in our database --> If found; can successfully authenticate the user and log them in
       .then(user => {
-        if (!user) {
+        if (!user) { //If inputted username doesn't exist
           const err = new Error(`User ${username} does not exist!`);
           err.status = 401;
           return next(err);
-        } else if (user.password !== password) {
+        } else if (user.password !== password) { //If inputted password doesn't match the docs in our database
           const err = new Error('Your password is incorrect!');
           err.status = 401;
           return next(err);
-        } else if (user.username === username && user.password === password) {
+        } else if (user.username === username && user.password === password) { //Both username and password are correct: 
           req.session.user = 'authenticated';
           res.statusCode = 200;
           res.setHeader('Content-Type', 'text/plain');
@@ -64,19 +68,20 @@ router.post('/login', (req, res, next) => {
         }
       })
       .catch(err => next(err));
-  } else {
+  } else {//Client is already logged in 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('You are already authenticated!');
   }
 });
 
+//USER LOGOUT
 router.get('/logout', (req, res, next) => {
   if (req.session) {
-    req.session.destroy();
-    res.clearCookie('session-id');
-    res.redirect('/');
-  } else {
+    req.session.destroy(); //destroying session: deleting session file on server side
+    res.clearCookie('session-id');//Clearing cookie stored on client
+    res.redirect('/');//Redirecting user to root path
+  } else { //If session doesn't exist (Client is requesting to logout without being logged in)
     const err = new Error('You are not logged in!');
     err.status = 401;
     return next(err);
