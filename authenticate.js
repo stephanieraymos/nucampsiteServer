ef2a6640671b906a07410ea94c6b18b852e6e652
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-16 20:38:07 -0700

    user comments

--- authenticate.js
+++ authenticate.js
@@ -2,6 +2,6 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 
-exports.local = passport.use(new LocalStrategy(User.authenticate()));
+exports.local = passport.use(new LocalStrategy(User.authenticate())); //Verifying the username and pass against the locally stored user and pass
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
