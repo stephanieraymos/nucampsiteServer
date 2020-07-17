156ce43e0f42cbacb8eea7d1d093cb20c020d9a4
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-16 20:46:06 -0700

    user login comments

--- routes/users.js
+++ routes/users.js
@@ -30,42 +30,12 @@ router.post('/signup', (req, res) => { //Allows new user to register on this web
   );
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', passport.authenticate('local'), (req, res) => { //It's possible to insert multiple middleware functions in a routing method.
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({ success: true, status: 'You are successfully logged in!' });
 });
 
-
-//HANDLE USER LOGIN
-const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-const username = auth[0];
-const password = auth[1];
-
-User.findOne({ username: username }) //Checking inputted username against the user docs that we have in our database --> If found; can successfully authenticate the user and log them in
-  .then(user => {
-    if (!user) { //If inputted username doesn't exist
-      const err = new Error(`User ${username} does not exist!`);
-      err.status = 401;
-      return next(err);
-    } else if (user.password !== password) { //If inputted password doesn't match the docs in our database
-      const err = new Error('Your password is incorrect!');
-      err.status = 401;
-      return next(err);
-    } else if (user.username === username && user.password === password) { //Both username and password are correct: 
-      req.session.user = 'authenticated';
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/plain');
-      res.end('You are authenticated!')
-    }
-  })
-  .catch(err => next(err));
-  } else {//Client is already logged in 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('You are already authenticated!');
-}
-});
 
 //USER LOGOUT
 router.get('/logout', (req, res, next) => {
@@ -80,4 +50,5 @@ router.get('/logout', (req, res, next) => {
   }
 });
 
+//passport.authenticate handles challenging user for credentials, parsing the credentials from the req body ect.
 module.exports = router;
