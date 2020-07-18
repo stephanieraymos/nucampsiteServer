678a21b437b8f959de89795f80893a0e14ddb0de
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-17 17:41:50 -0700

    Removed unneeded sessions code

--- app.js
+++ app.js
@@ -44,39 +44,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
-app.use(session({
-  name: 'session-id', //Can be named anything
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false, //--> Helps prevent having a bunch of empty session files and cookies being set up. --When a new session is created; but then no updates are made to it: then at the end of the req it won't get saved because it would just be an empty session + no cookie would be sent to the client.
-  resave: false, //--> Helps keep the session marked as active so it doesn't get deleted. --Once a session has been created, updated and saved; it will continue to be resaved whenever a req is made for that session; even if that req didn't make any updates. 
-  store: new FileStore() //Saves session to server's hard disk instead of just in running app memory. 
-}));
+
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 //ROUTES 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) { //If incoming req doesn't include the session.user property or if session value is false --> client hasn't been authenticated.
-    const err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
-  } else {
-    return next();
-  }
-}
-
-app.use(auth);
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 //ROUTERS
-
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
