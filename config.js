a8de8931fa589317887f959cadc3b71a8d529315
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-21 18:58:33 -0700

    Added id and secret for fb login to config

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 module.exports = {
   MONGO_URI: 'mongodb+srv://nucamp:jEVtgpeVNegvMSlj@cluster0.vykvt.mongodb.net/<dbname>?retryWrites=true&w=majority', //MDL
   'secretKey': '12345-67890-09876-54321', //Used to sign json web token
-  'mongoUrl': 'mongodb://localhost:27017/nucampsite' //url for mongodb server
+  'mongoUrl': 'mongodb://localhost:27017/nucampsite', //url for mongodb server
+  'facebook': {
+    clientId: '211553863502914',
+    clientSecret: '9d68ccc0154ab27cd50edd6492e8d9c9'
+  }
 }
