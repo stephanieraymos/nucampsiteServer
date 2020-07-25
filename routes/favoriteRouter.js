a9ac30660e57dd1761c94b803620ea999b0624e5
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-25 11:11:48 -0700

    Fixed Favorite.find to req user id and populate user and campsites seperately

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -7,77 +7,78 @@ const cors = require('./cors'); //CORS module created in the routes folder
 favoriteRouter.use(bodyParser.json());
 
 favoriteRouter.route('/')
-    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200)) //Handling preflight req. --> Any time a client needs to preflight a req: it will do so by sending a req with the http options method. Client will wait for server to respond with info on what kind of req it will accept to figure out whether or not it can send it's actual req.
-    .get(cors.cors, (req, res, next) => {
-        Favorite.find()
-            .populate('user.campsites') 
-            .then(favorite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(favorite);
-            })
-            .catch(err => next(err));
-    })
-    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-        Favorite.create(req.body) //Mongoose will let us know if we're missing any data in the request body
-            .then(favorite => {
-                console.log('Campsite has been added to favorites', campsite); //Second argument; campsite: will log info about the campsite to the console.
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(favorite); //Sends info about posted document to the client. (No res.end needed)
-            })
-            .catch(err => next(err));
-    })
-    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
-        res.statusCode = 403;
-        res.end('PUT operation not supported on /favprites');
-    })
-    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-        Favorite.deleteMany() //Every Favorite will be deleted
-            .then(response => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(response);
-            })
-            .catch(err => next(err));
-    });
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200)) //Handling preflight req. --> Any time a client needs to preflight a req: it will do so by sending a req with the http options method. Client will wait for server to respond with info on what kind of req it will accept to figure out whether or not it can send it's actual req.
+  .get(cors.cors, (req, res, next) => {
+    Favorite.find({ user: req.user._id })
+      .populate('user')
+      .populate('campsites')
+      .then(favorite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(favorite);
+      })
+      .catch(err => next(err));
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.create(req.body) //Mongoose will let us know if we're missing any data in the request body
+      .then(favorite => {
+        console.log('Campsite has been added to favorites', campsite); //Second argument; campsite: will log info about the campsite to the console.
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(favorite); //Sends info about posted document to the client. (No res.end needed)
+      })
+      .catch(err => next(err));
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /favprites');
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.deleteMany() //Every Favorite will be deleted
+      .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+      })
+      .catch(err => next(err));
+  });
 
 favoriteRouter.route('/:campsiteId')
-    .options(cors.corsWithOptions, (req, res) => res.sendStatus(200)) //Handling preflight req. --> Any time a client needs to preflight a req: it will do so by sending a req with the http options method. Client will wait for server to respond with info on what kind of req it will accept to figure out whether or not it can send it's actual req.
-    .get(cors.cors, (req, res, next) => {
-        Campsite.findById(req.params.campsiteId) //this id is getting parsed from the http request; from whatever the user on client side typed in as the id they want to access
-            .populate('user.campsites')
-            .then(favorite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(favorite);
-            })
-            .catch(err => next(err));
-    })
-    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-        res.statusCode = 403;
-        res.end(`POST operation not supported on /favorites/${req.params.campsiteId}`);
-    })
-    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
-        Favorite.findByIdAndUpdate(req.params.campsiteId, {
-            $set: req.body //update operator along with the data in the request body
-        }, { new: true }) //this is so we get back info about the updated document resulting from this method
-            .then(favorite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(favorite);
-            })
-            .catch(err => next(err));
-    })
-    .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-        Favorite.findByIdAndDelete(req.params.campsiteId)
-            .then(response => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(response);
-            })
-            .catch(err => next(err));
-    });
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200)) //Handling preflight req. --> Any time a client needs to preflight a req: it will do so by sending a req with the http options method. Client will wait for server to respond with info on what kind of req it will accept to figure out whether or not it can send it's actual req.
+  .get(cors.cors, (req, res, next) => {
+    Campsite.findById(req.params.campsiteId) //this id is getting parsed from the http request; from whatever the user on client side typed in as the id they want to access
+      .populate('user.campsites')
+      .then(favorite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(favorite);
+      })
+      .catch(err => next(err));
+  })
+  .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /favorites/${req.params.campsiteId}`);
+  })
+  .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    Favorite.findByIdAndUpdate(req.params.campsiteId, {
+      $set: req.body //update operator along with the data in the request body
+    }, { new: true }) //this is so we get back info about the updated document resulting from this method
+      .then(favorite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(favorite);
+      })
+      .catch(err => next(err));
+  })
+  .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    Favorite.findByIdAndDelete(req.params.campsiteId)
+      .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+      })
+      .catch(err => next(err));
+  });
 
 
 module.exports = favoriteRouter;
