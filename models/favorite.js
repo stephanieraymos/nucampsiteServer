44f2c399dac4c7c7284b97fce98cbded4381b1e1
Stephanie Raymos <stephanieraymos@gmail.com>
2020-07-25 10:25:27 -0700

    favoriteSchema

---
+++ models/favorite.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+//This is making a shorthand to the mongoose.Schema function so we can refer to it as Schema:
+const Schema = mongoose.Schema;
+
+//Creating Schema:
+const favoriteSchema = new Schema({
+  user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Favorite,
+  },
+  campsites: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Favorite
+  }]
+}, {
+  timestamps: true //created at and updated at
+});
+
+//Creating Model: 
+//First argument: Capitalized and singular version of the collection you want to use for this model; Campsite for campsites collection
+//Second argument: Schema we want to use for this collection) 
+//Returns a constructor function (A de-sugared class)
+//Used to instantiate documents for mondoDB
+const Favorite = mongoose.model('Campsite', favoriteSchema);
+
+module.exports = Favorite;
